59ef2e5afade237b3ac56f22153f209077e0f17f
adolfobjj <luizadolfo7@gmail.com>
2022-09-21 13:14:07 -0300

    Atualizando a classe Usuário

--- src/main/java/br/com/desafio/internal/domain/Usuario.java
+++ src/main/java/br/com/desafio/internal/domain/Usuario.java
@@ -3,8 +3,13 @@ package br.com.desafio.internal.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @NoArgsConstructor
@@ -14,9 +19,17 @@ import java.io.Serializable;
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Integer id;
+    @NotEmpty(message = "Campo nome é mandatório")
+    @Length(min = 4, max = 20, message = "Entre 4 e 20 caracteres.")
     private String nome;
+    @NotEmpty(message = "Campo login é mandatório")
+    @Length(min = 4, max = 20, message = "Entre 4 e 20 caracteres.")
     private String login;
+    @NotEmpty(message = "Campo senha é mandatório")
+    @Length(min = 4, max = 20, message = "Entre 4 e 20 caracteres.")
     private String senha;
 
 }
