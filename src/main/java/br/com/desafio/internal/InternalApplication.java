21d61786f0db38df75546a6a601b98d03647aca2
adolfobjj <luizadolfo7@gmail.com>
2022-09-21 13:57:09 -0300

    H2 testado e dois usuarios estanciados

--- src/main/java/br/com/desafio/internal/InternalApplication.java
+++ src/main/java/br/com/desafio/internal/InternalApplication.java
@@ -1,13 +1,31 @@
 package br.com.desafio.internal;
 
+import br.com.desafio.internal.domain.Usuario;
+import br.com.desafio.internal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class InternalApplication {
+public class InternalApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(InternalApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario u1 = new Usuario(null, "Luiz Adolfo", "Luiz", "123456" );
+		Usuario u2 = new Usuario(null, "Felipe Augusto", "Felipe", "123456" );
+
+		usuarioRepository.saveAll(Arrays.asList(u1,u2));
+	}
 }
