61eb4d1b30a5a6159530d0ffcb9717a11267b184
adolfobjj <luizadolfo7@gmail.com>
2022-09-22 13:32:02 -0300

    Correção no ControllerExceptionHandler

--- src/main/java/br/com/desafio/internal/controller/ControllerExceptionHandler.java
+++ src/main/java/br/com/desafio/internal/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.desafio.internal.controller;
 
+import br.com.desafio.internal.service.exception.DataIntegratyViolationException;
 import br.com.desafio.internal.service.exception.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,16 +8,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e , HttpServletRequest request){
-
-        StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(),System.currentTimeMillis(), e.getMessage());
+    public ResponseEntity<StandardError>objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
+        StandardError error =
+                new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegratyViolationException ex, HttpServletRequest request) {
+        StandardError error =
+                new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 }
