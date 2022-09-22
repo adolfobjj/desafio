42b0035a5ca6488282bda2fb72bd9775408f8416
adolfobjj <luizadolfo7@gmail.com>
2022-09-22 10:40:24 -0300

    atualização controller

--- src/main/java/br/com/desafio/internal/controller/UserController.java
+++ src/main/java/br/com/desafio/internal/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/usuario")
 public class UserController {
 
     private static final String ID = "/{id}";
