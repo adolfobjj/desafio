ac5f2536c10760fc0c09b27ecbe72138de01b0b6
adolfobjj <luizadolfo7@gmail.com>
2022-09-22 18:15:54 -0300

    feat: criação do Repository

--- src/main/java/br/com/desafio/internal/repository/UserRepository.java
+++ src/main/java/br/com/desafio/internal/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+//classe repository
 @Repository
 public interface UserRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByEmail(String email);
