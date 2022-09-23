b30a88b38403497f00f84a7526a3cfcb4d26ec0f
adolfobjj <luizadolfo7@gmail.com>
2022-09-23 09:57:48 -0300

    feat: teste metodo delete com exceção

--- src/test/java/br/com/desafio/internal/service/Impl/UserServiceImplTest.java
+++ src/test/java/br/com/desafio/internal/service/Impl/UserServiceImplTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,7 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -145,7 +146,11 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        Mockito.verify(repository, times(1)).deleteById(anyInt());
     }
 
     private void startUser(){
